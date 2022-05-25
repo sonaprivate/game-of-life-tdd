db8bfe1dec6f62a82b6c3c76ce9a50f29b9546cb
suay-selay <suay_999@hotmail.com>
2022-05-25 22:02:16 +0400

    adding several methods based on rules

--- src/main/java/me/suayselay/gameOfLife/Cell.java
+++ src/main/java/me/suayselay/gameOfLife/Cell.java
@@ -2,6 +2,9 @@ package me.suayselay.gameOfLife;
 
 import lombok.*;
 
+import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;
+import static me.suayselay.gameOfLife.Cell.CellState.DEAD;
+
 @Getter
 @Setter
 @AllArgsConstructor
@@ -13,6 +16,24 @@ public class Cell {
     public enum CellState {
         ALIVE,
         DEAD
+    }
+
+    // adding several methods based on rules: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
+
+    public void getCellNextStateHavingAliveNeighbours(int numberOfAliveNeigbours) {
+        cellState = canLive(numberOfAliveNeigbours) ? ALIVE : DEAD;
+    }
+ 
+    private boolean canLive(int numberOfAliveNeigbours) {
+        return isReproductionPossible(numberOfAliveNeigbours) || (!isCellHasOverPopulatedORUnderPopulatedNeighbourHood(numberOfAliveNeigbours) && cellSate == ALIVE);
+    }
+ 
+    private boolean isReproductionPossible(int numberOfAliveNeigbours) {
+        return cellState == DEAD && numberOfAliveNeigbours == 3;
+    }
+ 
+    private boolean isCellHasOverPopulatedORUnderPopulatedNeighbourHood(int numberOfAliveNeigbours) {
+        return numberOfAliveNeigbours != 2 && numberOfAliveNeigbours != 3;
     }
     
 }
