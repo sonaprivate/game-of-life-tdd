eeb35eb425c26a3ff14a37051ff929f9e5d3b818
suay-selay <suay_999@hotmail.com>
2022-05-25 22:25:08 +0400

    Live Cell with more than 3 live neighbors will die

--- src/test/java/me/suayselay/gameOfLife/CellTest.java
+++ src/test/java/me/suayselay/gameOfLife/CellTest.java
@@ -7,7 +7,7 @@ import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;
 import static me.suayselay.gameOfLife.Cell.CellState.DEAD;
 
 public class CellTest {
-    
+
     @Test
     public void shouldHaveStateLiveWhenAlive() {
         Cell cell = new Cell(ALIVE);
@@ -20,7 +20,8 @@ public class CellTest {
         Assert.assertEquals(DEAD, cell.getCellState());
     }
 
-    // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    // Any live cell with fewer than two live neighbours dies, as if by
+    // underpopulation.
     @Test
     public void should_be_dead_by_underpopulation_when_live_cell_has_less_than_two_alive_neighbours() {
         Cell cell = new Cell(ALIVE);
@@ -29,7 +30,8 @@ public class CellTest {
         Assert.assertEquals(DEAD, cell.getCellState());
     }
 
-    // Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with two or three live neighbours lives on to the next
+    // generation.
     @Test
     public void shouldBeAliveWhenLiveCellHasGreaterThanTwoAliveNeighbors() {
         Cell cell = new Cell(ALIVE);
@@ -38,7 +40,8 @@ public class CellTest {
         Assert.assertEquals(ALIVE, cell.getCellState());
     }
 
-    //Any live cell with two or three live neighbours lives on to the next generation.
+    // Any live cell with two or three live neighbours lives on to the next
+    // generation.
     @Test
     public void shouldBeAliveWhenLiveCellHasExactlyTwoOrThreeAliveNeighbors() {
         Cell cell = new Cell(ALIVE);
@@ -47,5 +50,13 @@ public class CellTest {
         Assert.assertEquals(ALIVE, cell.getCellState());
     }
 
+    // Any live cell with more than three live neighbours dies, as if by overpopulation.
+    @Test
+    public void should_be_dead_by_overpopulation_when_live_cell_has_more_than_three_alive_neighbours() {
+        Cell cell = new Cell(ALIVE);
+        int numberOfAliveNeigbours = 4;
+        cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
+        Assert.assertEquals(DEAD, cell.getCellState());
+    }
 
 }
