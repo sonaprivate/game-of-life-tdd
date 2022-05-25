47e921715451cab0664befb54c623ed449b068db
suay-selay <suay_999@hotmail.com>
2022-05-25 22:44:03 +0400

    renaming method names to camel case

--- src/test/java/me/suayselay/gameOfLife/CellTest.java
+++ src/test/java/me/suayselay/gameOfLife/CellTest.java
@@ -22,7 +22,7 @@ public class CellTest {
 
     // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
     @Test
-    public void should_be_dead_by_underpopulation_when_live_cell_has_less_than_two_alive_neighbours() {
+    public void shouldBeDeadByUnderPopulationWhenLiveCellHasLessThanTwoAliveNeighbors() {
         Cell cell = new Cell(ALIVE);
         int numberOfAliveNeigbours = 1;
         cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
@@ -49,7 +49,7 @@ public class CellTest {
 
     // Any live cell with more than three live neighbours dies, as if by overpopulation.
     @Test
-    public void should_be_dead_by_overpopulation_when_live_cell_has_more_than_three_alive_neighbours() {
+    public void shouldBeDeadByOverPopulationWhenLiveCellHasMoreThanThreeAliveNeighbors() {
         Cell cell = new Cell(ALIVE);
         int numberOfAliveNeigbours = 4;
         cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
@@ -58,7 +58,7 @@ public class CellTest {
 
     //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
     @Test
-    public void should_be_alive_by_reproduction_when_dead_cell_has_three_alive_neighbours() {
+    public void shouldBeAliveByReproductionWhenDeadCellHasThreeAliveNeighbors() {
         Cell cell = new Cell(DEAD);
         int numberOfAliveNeigbours = 3;
         cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
@@ -68,7 +68,7 @@ public class CellTest {
 
     //Any Dead cell with more than three live neighbours dies, as if by overpopulation.
     @Test
-    public void should_be_still_dead_when_dead_cell_has_more_than_three_alive_neighbours() {
+    public void shouldBeStillDeadWhenDeadCellHasMoreThanThreeAliveNeighbors() {
         Cell cell = new Cell(DEAD);
         int numberOfAliveNeigbours = 4;
         cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
@@ -77,7 +77,7 @@ public class CellTest {
 
     //Any dead cell with exactly two live neighbours becomes a dead cell
     @Test
-    public void should_be_still_dead_when_dead_cell_has_less_than_three_alive_neighbours() {
+    public void shouldBeStillDeadWhenDeadCellHasLessThanThreeAliveNeighbors() {
         Cell cell = new Cell(DEAD);
         int numberOfAliveNeigbours = 2;
         cell.getCellNextStateHavingAliveNeighbours(numberOfAliveNeigbours);
