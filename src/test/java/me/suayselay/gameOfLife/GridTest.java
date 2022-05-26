2210d1d6b6914844a2a1cbecb3213cef6717fde8
suay-selay <suay_999@hotmail.com>
2022-05-26 14:18:02 +0400

    testing getAliveCount method

--- src/test/java/me/suayselay/gameOfLife/GridTest.java
+++ src/test/java/me/suayselay/gameOfLife/GridTest.java
@@ -16,6 +16,7 @@ public class GridTest {
     public static final Cell.CellState X = ALIVE;
     public static final Cell.CellState O = DEAD;
 
+    // check for if input correctly pass to grid
     @Test
     public void shouldBeInitiatedWithInitialState() {
         Cell.CellState[][] input = new Cell.CellState[][] {
@@ -27,6 +28,21 @@ public class GridTest {
         Grid grid = new Grid(input);
         Cell.CellState[][] state = grid.getCellsState();
         Assert.assertArrayEquals(input, state);
+    }
+
+    // check if getAliveCount works correctly or not
+    @Test
+    public void shouldBeAbleToDeterminaNumberOfAliveCells() {
+        Cell.CellState[][] input = new Cell.CellState[][] {
+                { O, O, O, O, O, O, O, O },
+                { O, O, O, O, X, O, O, O },
+                { O, O, O, X, X, O, O, O },
+                { O, O, O, O, O, O, O, O }
+        };
+        Grid grid = new Grid(input);
+        int expectedNumberOfAliveCells = 3;
+        int actualAliveCells = grid.getAliveCount();
+        assertEquals(expectedNumberOfAliveCells, actualAliveCells);
     }
 
 }
