8a41cc9db723d3a482d4aed57ab10d36d55f8de2
suay-selay <suay_999@hotmail.com>
2022-05-26 12:57:47 +0400

    Initialising Coordinate with row and column

--- src/main/java/me/suayselay/gameOfLife/Coordinate.java
+++ src/main/java/me/suayselay/gameOfLife/Coordinate.java
@@ -1,5 +1,14 @@
 package me.suayselay.gameOfLife;
 
+import lombok.*;
+
+@Getter
+@AllArgsConstructor
 public class Coordinate {
+
+    private final int row;
+    private final int col;
+
+    
     
 }
