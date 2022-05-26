8e0ab41e474698e53cc50dbb6370768ec16c97f3
suay-selay <suay_999@hotmail.com>
2022-05-26 13:21:29 +0400

    adding method for getting next generation of cells

--- src/main/java/me/suayselay/gameOfLife/Grid.java
+++ src/main/java/me/suayselay/gameOfLife/Grid.java
@@ -1,6 +1,6 @@
 package me.suayselay.gameOfLife;
 
-import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;;
+import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;
 
 import java.util.List;
 
@@ -62,5 +62,38 @@ public class Grid {
             }
         }
         return count;
+    }
+
+
+    // Method for getting next generation of Cells
+    public Grid getNextGeneration() {
+ 
+        Grid newGrid = new Grid(getCellsState());
+        for (int row = 0; row < cells.length; row++) {
+            for (int col = 0; col < cells[row].length; col++) {
+                Cell cell = cells[row][col];
+                List<Coordinate> neighbours = new Coordinate(row, col).getNeighbours(this);
+                int liveNeighbourCount = getAliveCountOf(neighbours);
+                cell.getCellNextStateHavingAliveNeighbours(liveNeighbourCount);
+                newGrid.getCells()[row][col].setCellState(cell.getCellState());
+            }
+        }
+        return newGrid;
+    }
+ 
+    @Override
+    public String toString() {
+        StringBuilder buffer = new StringBuilder();
+        for (Cell[] cell : cells) {
+            for (Cell value : cell) {
+                if (isCellAlive(value)) {
+                    buffer.append("*");
+                } else {
+                    buffer.append(".");
+                }
+            }
+            buffer.append("\n");
+        }
+        return buffer.toString();
     }
 }
