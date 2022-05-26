80618256ba7005c4918296d84dbfa6369c04dc6a
suay-selay <suay_999@hotmail.com>
2022-05-26 14:16:00 +0400

    check if entered state correctly pass to grid

--- src/test/java/me/suayselay/gameOfLife/GridTest.java
+++ src/test/java/me/suayselay/gameOfLife/GridTest.java
@@ -4,18 +4,29 @@ import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;
 import static me.suayselay.gameOfLife.Cell.CellState.DEAD;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
- 
+
 import java.util.ArrayList;
 import java.util.List;
- 
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class GridTest {
-    
+
     public static final Cell.CellState X = ALIVE;
     public static final Cell.CellState O = DEAD;
 
-    
+    @Test
+    public void shouldBeInitiatedWithInitialState() {
+        Cell.CellState[][] input = new Cell.CellState[][] {
+                { O, O, O, O, O, O, O, O },
+                { O, O, O, O, X, O, O, O },
+                { O, O, O, X, X, O, O, O },
+                { O, O, O, O, O, O, O, O }
+        };
+        Grid grid = new Grid(input);
+        Cell.CellState[][] state = grid.getCellsState();
+        Assert.assertArrayEquals(input, state);
+    }
 
 }
