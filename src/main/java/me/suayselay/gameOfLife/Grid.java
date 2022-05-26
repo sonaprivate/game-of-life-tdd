392f4004c89802ddb39db8c96bcb4dc4108658a1
suay-selay <suay_999@hotmail.com>
2022-05-26 13:13:26 +0400

    initialising constructor with the whole cells

--- src/main/java/me/suayselay/gameOfLife/Grid.java
+++ src/main/java/me/suayselay/gameOfLife/Grid.java
@@ -1,5 +1,16 @@
 package me.suayselay.gameOfLife;
 
 public class Grid {
-    
+    private Cell[][] cells;
+
+    // initialising constructor with the whole cells in the grid
+    public Grid(Cell.CellState[][] input) {
+        cells = new Cell[input.length][];
+        for (int row = 0; row < input.length; row++) {
+            cells[row] = new Cell[input[row].length];
+            for (int col = 0; col < input[row].length; col++) {
+                cells[row][col] = new Cell(input[row][col]);
+            }
+        }
+    }
 }
