5a3a043ad17cadc615e5e7a354d7aac05e06b8c9
suay-selay <suay_999@hotmail.com>
2022-05-26 14:13:49 +0400

    adding and initialising GridTest class

---
+++ src/test/java/me/suayselay/gameOfLife/GridTest.java
@@ -0,0 +1,21 @@
+package me.suayselay.gameOfLife;
+
+import static me.suayselay.gameOfLife.Cell.CellState.ALIVE;
+import static me.suayselay.gameOfLife.Cell.CellState.DEAD;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GridTest {
+    
+    public static final Cell.CellState X = ALIVE;
+    public static final Cell.CellState O = DEAD;
+
+    
+
+}
